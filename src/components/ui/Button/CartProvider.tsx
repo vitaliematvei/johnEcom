a39a2c5f3aeeff1f52b23cae1d4d897cac033b51
Matvei Vitalie <vitaliematvei@gmail.com>
2025-07-28 13:38:43 +0300

    fixed stripe

--- src/components/ui/Button/CartProvider.tsx
+++ src/components/ui/Button/CartProvider.tsx
@@ -1,15 +1,15 @@
-"use client";
+'use client';
 
-import { CartProvider as CProvider } from "use-shopping-cart";
+import { CartProvider as CProvider } from 'use-shopping-cart';
 
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
   return (
     <CProvider
       mode="payment"
       cartMode="client-only"
-      stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}
+      stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY ?? ''}
       successUrl="/success"
       cancelUrl="/error"
       currency="USD"
